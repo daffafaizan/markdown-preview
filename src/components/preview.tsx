3fa3a8dfd2982c98280e386d8be6758fab91f5fe
Daffa <daffamfzn@gmail.com>
2024-01-27 13:08:02 +0700

    feat: added react markdown and remark gfm plugin for markdown

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,9 +1,15 @@
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
 export default function Preview({ markdown }: { markdown: string }) {
   return (
     <div className="h-full w-full flex flex-col items-center justify-center bg-red-500 p-4">
-      <div className="h-full w-full rounded-lg bg-purple-500 p-4">
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        className="h-full w-full rounded-lg bg-purple-500 p-4"
+      >
         {markdown}
-      </div>
+      </Markdown>
     </div>
   );
 }
