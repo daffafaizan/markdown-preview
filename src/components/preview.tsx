2c9c6f49dc11c40b42934857a976bf63a8a76386
Daffa <daffamfzn@gmail.com>
2024-01-27 13:20:32 +0700

    feat: added syntax highlighter for code highlighting

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,12 +1,34 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function Preview({ markdown }: { markdown: string }) {
   return (
     <div className="h-full w-full flex flex-col items-center justify-center bg-red-500 p-4">
       <Markdown
-        remarkPlugins={[remarkGfm]}
         className="h-full w-full rounded-lg bg-purple-500 p-4"
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code(props) {
+            const { children, className, node, ...rest } = props as any;
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                language={match[1]}
+                style={dark}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
       >
         {markdown}
       </Markdown>
